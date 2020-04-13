24bafe5ad82a15d76155b6d95dd735e6d998c3db
Emil <dancescu_emilian@yahoo.com>
2020-04-14 00:02:56 +0300

    exported Authorize component

--- src/lib.js
+++ src/lib.js
@@ -3,10 +3,11 @@ export { default as Admin } from 'Admin'
 
 export { default as net } from 'utils/net'
 
+export { default as Authorize } from 'components/core/Authorize'
+export { default as DetailsModal } from 'components/core/DetailsModal'
+
 export { default as ActionButton } from 'components/generic/ActionButton'
 export { default as DataTable } from 'components/generic/DataTable'
 export { default as Form } from 'components/generic/Form'
 export { default as Map } from 'components/generic/Map'
 export { default as QRScanner } from 'components/generic/QRScanner'
-
-export { default as DetailsModal } from 'components/core/DetailsModal'
