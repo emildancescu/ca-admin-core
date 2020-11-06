4aacdf40c44d1b9c4ff3058da271f63ca7ca93ef
Toderita G. Cristian <48821680+toderitagcristian@users.noreply.github.com>
2020-11-06 11:46:08 +0200

    CL-2584 CL-2583 (#8)
    CL-2584 Implement remoteFIlter for DataTable
    CL-2583 Refactor select, remoteSelect

---
+++ src/components/generic/DataTable/remoteFilter.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import { Select, Spin } from 'antd'
+import debounce from 'lodash/debounce'
+import isObject from 'lodash/isObject'
+
+const { Option } = Select
+
+class RemoteFilter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.fetchData = debounce(this.fetchData, 800)
+  }
+
+  state = {
+    data: [],
+    value: [],
+    fetching: false,
+  }
+
+  fetchData = async value => {
+    if (!value) {
+      return
+    }
+
+    this.setState({ data: [], fetching: true })
+
+    const {
+      remoteSearch: { apiFn, paramSearchQuery },
+    } = this.props
+
+    const params = {}
+    params[paramSearchQuery] = value
+
+    const data = await apiFn(params)
+
+    this.setState({ data, fetching: false })
+  }
+
+  handleChange = value => {
+    const {
+      tableDropdownFn: { setSelectedKeys, confirm },
+    } = this.props
+
+    setSelectedKeys(value ? [value] : null)
+
+    confirm()
+
+    this.setState({
+      value,
+      data: [],
+      fetching: false,
+    })
+  }
+
+  render() {
+    // eslint-disable-next-line no-unused-vars
+    const { fetching, data, value } = this.state
+
+    const {
+      remoteSearch: { apiFn, paramSearchQuery, styleSelect, ...rest },
+    } = this.props
+
+    return (
+      <Select
+        {...rest}
+        // DO NOT modify below default props
+        style={styleSelect ? { ...styleSelect } : { width: '100%', marginBottom: 8, marginTop: 8 }}
+        showSearch
+        onSearch={this.fetchData}
+        onChange={this.handleChange}
+        onFocus={() => this.fetchData(' ')}
+        notFoundContent={fetching ? <Spin size="small" /> : null}
+      >
+        {data.map(d => {
+          const isString = !isObject(d)
+
+          return (
+            <Option key={d} value={isString ? d : d.value} title={isString ? d : d.text}>
+              {isString ? d : d.text}
+            </Option>
+          )
+        })}
+      </Select>
+    )
+  }
+}
+
+export default RemoteFilter
--- src/testModules/dashboard/pages/tableSortFix.js
+++ src/testModules/dashboard/pages/tableSortFix.js
@@ -1,9 +1,71 @@
 import React from 'react'
-import { DataTable } from 'lib'
+import { DataTable, net, getToken } from 'lib'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import { Card, Radio } from 'antd'
+import { Card, Radio, Tag } from 'antd'
 import { load } from '../list/actions'
+
+const { api, get } = net
+
+const API_USERS = `https://stage.api.citylink.ro/api/v1/admin/users`
+
+const getUsers = params => get(API_USERS, params, getToken())
+
+const remoteFilterUsersFirstName = {
+  filterOption: false,
+  mode: 'single',
+  paramSearchQuery: 'search',
+  allowClear: true,
+  styleColumn: { padding: 8, width: '200px' },
+  styleSelect: { width: '100%', marginBottom: 8, marginTop: 8 },
+  apiFn: async params => {
+    let result = []
+
+    params = {
+      ...params,
+      // limit: 5,
+    }
+
+    await api(getUsers(params))
+      .then(response => response.json())
+      .then(json => {
+        result = json.data.map(user => ({
+          text: `${user.id} - ${user.first_name}`,
+          value: user.id,
+        }))
+      })
+
+    return result
+  },
+}
+
+const remoteFilterUsersLastName = {
+  filterOption: false,
+  mode: 'single',
+  paramSearchQuery: 'search',
+  allowClear: true,
+  styleColumn: { padding: 8, width: '200px' },
+  styleSelect: { width: '100%', marginBottom: 8, marginTop: 8 },
+  apiFn: async params => {
+    let result = []
+
+    params = {
+      ...params,
+      // limit: 5,
+    }
+
+    await api(getUsers(params))
+      .then(response => response.json())
+      .then(json => {
+        result = json.data.map(user => ({
+          text: `${user.id} - ${user.last_name}`,
+          value: user.id,
+        }))
+      })
+
+    return result
+  },
+}
 
 @withRouter
 @connect(({ users }) => ({ users }))
@@ -27,8 +89,16 @@ class Table extends React.Component {
         sorter: true,
       },
       {
-        title: 'Roles',
+        title: 'roles',
         dataIndex: 'roles',
+        render: roles => roles.map(role => <Tag key={role}>{role}</Tag>),
+        filters: [
+          { text: 'client', value: 'client' },
+          { text: 'operator', value: 'operator' },
+          { text: 'bike-operator', value: 'bike-operator' },
+          { text: 'fleet-operator', value: 'fleet-operator' },
+          { text: 'admin', value: 'admin' },
+        ],
         mobile: false,
       },
     ]
@@ -65,11 +135,13 @@ class Table extends React.Component {
         title: 'first_name',
         dataIndex: 'first_name',
         sorter: true,
+        remoteFilter: remoteFilterUsersFirstName,
       },
       {
         title: 'last_name',
         dataIndex: 'last_name',
         sorter: true,
+        remoteFilter: remoteFilterUsersLastName,
       },
       {
         title: 'email',
