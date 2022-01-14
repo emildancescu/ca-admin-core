09fc1a8fc9bb16580d10b9a23fe2d00d6ff178d9
Emil Dancescu <dancescu_emilian@yahoo.com>
2022-01-14 18:44:30 +0200

    temporary english placeholders for uk / ru datatable translations

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -7,6 +7,13 @@ const messages = {
   'topBar.profileMenu.phone': 'Телефон',
   'topBar.profileMenu.refresh': 'Обновить',
   'topBar.profileMenu.logout': 'Выход',
+
+  'datatable.search': 'Search...',
+  'datatable.actions.refresh': 'Refresh',
+  'datatable.tooltips.clear': 'Clear all filters',
+  'datatable.tooltips.refresh': 'Reload using current filters',
+  'datatable.tooltips.columns': 'Toggle column visibility',
+  'datatable.tooltips.export': 'Export',
 }
 
 export default {
