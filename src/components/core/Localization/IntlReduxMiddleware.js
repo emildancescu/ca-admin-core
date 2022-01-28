384f05f576f197bedd3f22a81a4867d8a168a14e
Emil Dancescu <dancescu_emilian@yahoo.com>
2022-01-28 17:27:37 +0200

    Release 1.0.34 (#46)
    * CR-3345 Extend Redux INTL implementantion (#43)
    * fix missing keys for MenuLeft permissions debug containers (#45)
    
    Co-authored-by: Toderita G. Cristian <48821680+toderitagcristian@users.noreply.github.com>

--- src/components/core/Localization/IntlReduxMiddleware.js
+++ src/components/core/Localization/IntlReduxMiddleware.js
@@ -1,16 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 import { intl } from './intlSetup'
 
-export const IntlReduxMiddleware = () => {
-  return next => action => {
-    const enhancedAction = { ...action }
-
-    if (action.type.endsWith('/success') || action.type.endsWith('/error')) {
-      enhancedAction.intl = intl
-    }
-
-    const returnValue = next(enhancedAction)
-
-    return returnValue
-  }
-}
+export const IntlReduxMiddleware = () => next => action => next({ ...action, intl })
